bd6e1a8564670f1592053db50b9edd1059c0c663
YongliLiu2021 <yonglili2017@gmail.com>
2022-10-23 11:41:02 -0600

    init

---
+++ init.bat
@@ -0,0 +1,10 @@
+git init
+git config --global user.name "YongliLiu2021"
+git config --global user.email "yonglili2017@gmail.com"
+git config --global color.ui true
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+git config --global color.interactive auto
+git config --global --unset http.proxy
+git clone https://github.com/YongliLiu2021/hello-world.git
