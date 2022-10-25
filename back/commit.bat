766b1fb8c8299828bb54a81b33c2f86ca0b354e1
YongliLiu2021 <yonglili2017@gmail.com>
2022-10-25 10:24:18 -0600

    commit message

---
+++ back/commit.bat
@@ -0,0 +1,5 @@
+git pull -v --progress "origin"
+git add hello-world/
+git commit -m "commit xxxxx"
+git merge remotes/origin/main
+git push --progress "origin" main:main
---
+++ callCommit.bat
@@ -0,0 +1,4 @@
+git add hello-world/
+git commit -m "commit xxxxx"
+git merge remotes/origin/main
+
